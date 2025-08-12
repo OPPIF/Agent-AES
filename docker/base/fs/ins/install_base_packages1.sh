9cc36e358d057d8ad26b32e89ba7991902639c45
OPPIF <110786796+OPPIF@users.noreply.github.com>
2025-08-12 15:25:05 +0200

    Fix missing find and wc commands in Docker base packages

--- docker/base/fs/ins/install_base_packages1.sh
+++ docker/base/fs/ins/install_base_packages1.sh
@@ -6,6 +6,6 @@ echo "====================BASE PACKAGES1 START===================="
 apt-get update && apt-get upgrade -y
 
 apt-get install -y --no-install-recommends \
-    sudo curl wget git cron
+    sudo curl wget git cron findutils coreutils
 
 echo "====================BASE PACKAGES1 END===================="
